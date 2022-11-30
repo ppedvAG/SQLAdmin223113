544e85f560c1375b8a9ec17ebe096e9d30376f01
Rauchendraser <andreasr@ppedv.de>
2022-11-30 11:11:18 +0100

    Policies

---
+++ SQL Administration/SQL Administration/X_DB_Verkleinern.sql
@@ -0,0 +1,11 @@
+--Logfile zu groß?
+
+-- Model auf Einfach stellen
+--anschliessend DB Verkleinern oder Dateigrößen reduzieren
+
+--danach Dateien wieder auf anständiges Maß setzten: Logfile ca 25 -30% 
+--Wachstum ideal 1024 MB
+
+--> regelm Sichern;-)
+
+--> Model wieder auf Full setzen und eine Vollsicherung starten
